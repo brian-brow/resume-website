369f20b6ec13ffc5131a931394d4a262727ccb9d
Brian Brown <brianbrown13378@gmail.com>
2026-05-17 15:29:29 -0400

    navbar now sticky

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
   return (
-    <nav className="px-8 py-4 border-b border-gray-800 bg-gray-950/80 backdrop-blur-sm">
+    <nav className="px-8 py-4 border-b border-gray-800 bg-gray-950/80 backdrop-blur-sm sticky top-0 z-50">
       <span className="font-medium">Brian Brown</span>
     </nav>
   )
